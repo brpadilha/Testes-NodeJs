5feb9e713db3c6704c78dabeebdf788e4dcd80ec
root <root@DESKTOP-41CTVRE.localdomain>
2020-01-13 21:03:14 -0200

    Realiza testes de email duplicado

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
+import truncate from '../util/truncate';
 
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('Should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +17,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
